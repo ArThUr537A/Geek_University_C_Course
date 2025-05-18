c4d47bb700e7f1df5dd90de6389c8b078f5424ae
ArThUr537A <arthurzimermann0902@gmail.com>
2025-05-18 19:46:43 -0300

    18_05_2025

---
+++ Aulas/Aula_27_(vetores parte 1 )/Aula_27_1.c
@@ -0,0 +1,38 @@
+//vetores - parte 1
+//array uni-dimensional
+//char nome[50];
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    //vetores e strings
+    char nome[50];
+    // "dasdasdfasf"
+    printf("Qual seu nome? ");
+    gets(nome);
+    printf("Olá %s\n", nome);
+
+    //vetores e caracteres
+    char letras[26];
+    //'a'
+    int contador = 0;
+    for(int i = 97; i<= 122; i++){
+        letras[contador] = i;
+        contador = contador + 1;
+    }
+    //Imprimindo as letras e seus valores em decimal
+    for(int j = 0; j < 26; j++){
+        printf("%d == %c\n", letras[j], letras[j]);
+    }
+
+    //vetores de inteiros
+    //int numeros[10];
+
+    //vetores reais
+    //float valores[5];
+
+    return 0;
+}
