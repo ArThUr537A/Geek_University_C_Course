7b4d33408756c61cb17648cf1804d36e4e1eccbd
ArThUr537A <arthurzimermann0902@gmail.com>
2025-06-13 21:15:04 -0300

    13_06_2025

--- Exercícios/ex020/ex020.c
+++ Exercícios/ex020/ex020.c
@@ -1,41 +1,33 @@
-/*Faça um programa que receba do usuário o nome de um arquivo texto e um caractere. Mostre na tela 
-quantas vezes aquele caractere aparece dentro do arquivo.
-*/
-
+/*2. Faça um programa que receba do usuário 
+o nome de um arquivo texto e mostre na tela quantas linhas 
+este arquivo possui.*/
 #include <stdio.h>
 #include <locale.h>
 
 int main(){
     setlocale(LC_ALL, "Portuguese_Brazil");
-    //declara o documento e as variáveis.
     FILE* document_read;
     char document_name[10];
-    char caractere;
-    char caractere_lido;
     int contador = 0;
+    char caractere_lido;
 
-    //solicita o nome do documento e o caractere.
-    printf("Digite o nome do documento: ");
+    printf("Digite o nome do arquivo: ");
     gets(document_name);
-    printf("Digite um caractere: ");
-    scanf("%c", &caractere);
 
-    //abre o arquivo para leitura.
     document_read = fopen(document_name, "r");
-
-    //verifica se o arquivo pode ser lido.
     if(document_read){
-        //lê os caracteres e faz a contagem
         while((caractere_lido = getc(document_read)) != EOF){
-            if(caractere_lido == caractere){
+            if(caractere_lido == '\n'){
                 contador++;
             }
-        }  
+
+        }
+        
     }else{
-        printf("Não foi possível abrir o arquivo.");
+        printf("Não consegui ler o documento.");
     }
-    //imprime o resultado na tela
-    printf("O arquivo possui %d caracteres que são iguais ao caractere: %c", contador, caractere);
+    
+    printf("O arquivo possui %d linhas.", contador+1);
     fclose(document_read);
     return 0;
 }
