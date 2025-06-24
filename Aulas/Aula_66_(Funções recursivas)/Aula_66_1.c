97108e9bf49ed0eabe4ef4961060ecf2bcfee4d3
ArThUr537A <arthurzimermann0902@gmail.com>
2025-06-24 19:49:40 -0300

    24_06_2025

---
+++ Aulas/Aula_66_(Funções recursivas)/Aula_66_1.c
@@ -0,0 +1,35 @@
+# include <stdio.h>
+
+//Recursividade
+
+/*
+Recursividade é o ato de uma função chamar a si mesma
+Chamar a si mesma
+OBS: É importante que a função recursiva tenha uma
+condição de parada.
+*/
+
+int fib(int n){
+    if(n == 0){
+        return 0;
+    }
+
+    if(n == 1){
+        return 1;
+    }
+
+    return fib(n-1) + fib(n-2);
+}
+
+int main( void ){
+
+   int qtd;
+   printf("Informe o tamanho da sequencia fibonacci: ");
+   scanf("%d", &qtd);
+
+   for(int i=0; i<qtd; i++){
+      printf("%d ", fib(i+1));
+   } 
+
+   return 0;    
+}
