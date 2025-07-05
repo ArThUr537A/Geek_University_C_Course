d34fd3c6db95e71ebf42c27da2193dcc9e56d5c2
ArThUr537A <arthurzimermann0902@gmail.com>
2025-07-04 21:18:39 -0300

    04_07_2025

---
+++ Aulas/Aula_76_(stdlib.h)/Aula_76_1.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+
+//Conversão de Strings
+/* atof() converte string para double */
+/* atoi() converte uma string para inteiro */
+/* atol() converte string para inteiro longo*/
+
+//Geração de sequência pseudo-aleatória
+/* rand() gera um número aleatório */
+/* srand() inicializa o gerador de números aleatórios */
+
+//Gerenciamento de memória dinâmica
+/* malloc() */
+/* calloc() */
+/* free() */
+/* realoc() */
+
+//Pesquisa e ordenação
+/* qsort() ordena os elemento de um array */
+
+//Aritmética de inteiros
+/* abs() valor absoluto */
+/* div() divisão inteira */
