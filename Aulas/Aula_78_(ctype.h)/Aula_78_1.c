88186256035450073aa5c5417ab5c87ca67b43d5
ArThUr537A <arthurzimermann0902@gmail.com>
2025-07-06 19:26:16 -0300

    06_07_2025

---
+++ Aulas/Aula_78_(ctype.h)/Aula_78_1.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+
+// Teste de tipos de caracteres
+
+/* isalnum() verifica se o caractere é alfanumérico */
+/* isalpha() verifica se o caractere é alfabético */
+/* isdigit() verifica se o caractere é um dígito decimal */
+/* ispunct() verifica se o caractere é uma pontuação */
+/* isspace() verifica se o caractere é um espaço */
+/* isupper() verifica se o caractere é maisculo */
+/* tolower() converte o caractere para minúscula */
+/* toupper() converte o caractere para maiúscula */
