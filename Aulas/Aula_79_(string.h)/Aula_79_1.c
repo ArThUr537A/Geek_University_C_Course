c0cb9c82991c38fdb1385fa7d137d895831a1bc8
ArThUr537A <arthurzimermann0902@gmail.com>
2025-07-07 20:52:38 -0300

    07_07_2025

---
+++ Aulas/Aula_79_(string.h)/Aula_79_1.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+//Operações em Strings
+/* strcpy() copia de strings */
+/* strcat() concatena strings */
+/* strcmp() comparar duas strings */
+
+//Busca
+/* strchr() localiza a primeira ocorrencia de caractere em uma string */
+/* strtok() divide uma string em sub-strings com base em um caractere */
+/* strlen() retorna o tamanho da string */
+
