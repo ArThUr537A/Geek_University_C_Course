f9b95abc9cdaa6399c6671b8f8b37bc02a6bccf1
ArThUr537A <arthurzimermann0902@gmail.com>
2025-07-08 20:59:13 -0300

    08_07_2025

---
+++ Aulas/Aula_80_(time.h)/Aula_80_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+//localtime()
+
+int main(){
+   time_t tempobruto;
+   struct tm *info;
+
+   time(&tempobruto);
+ 
+   info = localtime(&tempobruto);
+   printf("Data e hora local %s", asctime(info));
+
+
+   return 0;    
+}
